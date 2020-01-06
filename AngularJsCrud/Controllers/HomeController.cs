efe5d149f84787b7bedd70609c20c10668eb1a0b
ask4brajesh <ask4brajesh@gmail.com>
2020-01-06 22:51:42 +0530

    asp.net mvc crud operation with angular js

---
+++ AngularJsCrud/Controllers/HomeController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AngularJsCrud.Models;
+
+namespace AngularJsCrud.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeEntities EmpDb = new EmployeeEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetAllEmployee()
+        {
+            string message = null; 
+            string status = null;
+            List<Employee> emp = new List<Employee>();
+            try {                
+                emp = EmpDb.Employee.ToList();
+                status = "ok";
+            }
+            catch (Exception ex) {
+                status = "failed";
+                message = ex.Message.ToString();
+            }
+
+            return Json( new { emp, status, message }, JsonRequestBehavior.AllowGet);
+           
+        }
+
+        public JsonResult GetEmployeeById(int Id) {
+            string message = null;
+            string status = null;
+            Employee emp = new Employee();
+            try
+            {
+                 emp = EmpDb.Employee.Find(Id);
+                status = "ok";
+            }
+            catch (Exception ex) {
+                status = "failed";
+                message = ex.Message;
+            }
+            return Json(new { emp, status, message }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult AddEmployee(Employee emp) {
+            string message = null;
+            string status = null;
+            try {
+                EmpDb.Employee.Add(emp);
+                EmpDb.SaveChanges();
+                status = "ok";
+                message = "Employee record saved sucessfully";
+            }
+            catch(Exception ex) {
+                status = "failed";
+                message = ex.Message;
+            }               
+            return Json( new { status, message }, JsonRequestBehavior.AllowGet);
+           
+        }
+
+        public JsonResult DeleteEmployeeById(int Id) {
+            string message = null;
+            string status = null;
+            Employee emp = new Employee();
+            try
+            {
+                emp = EmpDb.Employee.Find(Id);
+                EmpDb.Employee.Remove(emp);
+                EmpDb.SaveChanges();
+                status = "ok";
+                message = "Employee records deleted successfully";
+            }
+            catch (Exception ex) {
+                status = "failed";
+                message = ex.Message;
+            }
+            return Json( new {status, message }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public JsonResult UpdateEmployee(Employee emp) {
+            string message = null;
+            string status = null;
+            //Employee emp = new Employee();
+            try {
+                Employee obj = EmpDb.Employee.Where(x => x.Emp_Id == emp.Emp_Id).FirstOrDefault();
+                obj.Emp_Name = emp.Emp_Name;
+                obj.Emp_Age = emp.Emp_Age;
+                obj.Emp_City = emp.Emp_City;
+                EmpDb.SaveChanges();
+                status = "ok";
+                message = "Record Updated Successfully";
+            }
+            catch (Exception ex) {
+                status = "failed";
+                message = ex.Message;
+            }
+            return Json(new { status, message }, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+}
---
+++ AngularJsCrud/Scripts/Custom.js
@@ -0,0 +1,122 @@
+﻿/// <reference path="angular.js" />
+
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+    $scope.employees = null;
+    $scope.selectedId = null;
+    $scope.getAllData = function () {
+        $http({
+            method: "get",
+            url: "http://localhost:50204/Home/GetAllEmployee"
+        }).then(function (response) {
+            $scope.status = response.data.status;
+            if ($scope.status == "ok") {
+                $scope.employees = response.data.emp;
+                console.log($scope.employees);
+            } else {
+                alert("Records not found");
+            }
+            
+        }, function () {
+            alert("Error Occur");
+        })
+    };
+
+
+    $scope.InsertData = function () {
+        var Action = document.getElementById("btnSave").getAttribute("value");
+        if (Action == "Submit") {
+            var Employee = {};
+            Employee.Emp_Name = $scope.empName;
+            Employee.Emp_City = $scope.empCity;
+            Employee.Emp_Age = $scope.empAge;
+            $http({
+                method: "post",
+                url: "http://localhost:50204/Home/AddEmployee",
+                datatype: "json",
+                data: JSON.stringify(Employee)
+            }).then(function (response) {
+                $scope.status = response.data.status;
+                if ($scope.status == "ok") {
+                    $scope.message = response.data.message;
+                    //alert(response.data);
+                    $scope.getAllData();
+                    $scope.empName = "";
+                    $scope.empCity = "";
+                    $scope.empAge = "";
+                    alert($scope.message);
+                } else {
+                    alert(response.data.message);
+                }
+            })
+        } else {
+            var Employee = {};
+            Employee.Emp_Id = $scope.selectedId;
+            Employee.Emp_Name = $scope.empName;
+            Employee.Emp_City = $scope.empCity;
+            Employee.Emp_Age = $scope.empAge;
+            $http({
+                method: "post",
+                url: "http://localhost:50204/Home/UpdateEmployee",
+                datatype: "json",
+                data: JSON.stringify(Employee)
+            }).then(function (response) {
+                $scope.status = response.data.status;
+                if ($scope.status == "ok") {
+                    $scope.message = response.data.message;
+                    //alert(response.data);
+                    $scope.getAllData();
+                    document.getElementById("btnSave").setAttribute("value", "Submit"); 
+                    $scope.empName = "";
+                    $scope.empCity = "";
+                    $scope.empAge = "";
+                    alert($scope.message);
+                } else {
+                    alert(response.data.message);
+                }
+            })
+        }      
+       
+    };
+
+    $scope.UpdateEmp = function (emp) {
+        $scope.selectedId = emp.Emp_Id;
+        $scope.empName = emp.Emp_Name;
+        $scope.empCity = emp.Emp_City;
+        $scope.empAge = emp.Emp_Age;
+        document.getElementById("btnSave").setAttribute("value", "Update"); 
+    };
+
+
+
+    $scope.DeleteEmp = function (emp) {
+        if (emp != null) {
+            var Id = emp.Emp_Id;
+            $http({
+                method: "post",
+                url: "http://localhost:50204/Home/DeleteEmployeeById",
+                data: { Id: Id }
+            }).then(function (response) {
+                $scope.status = response.data.status;
+                if ($scope.status == "ok") {
+                    $scope.message = response.data.message;
+                    //alert(response.data);
+                    $scope.getAllData();
+                    alert($scope.message);
+                } else {
+                    alert(response.data.message);
+                }
+            })
+        } else {
+            alert("Please select employee from row");
+        }
+    }
+
+
+
+
+
+
+
+
+});
